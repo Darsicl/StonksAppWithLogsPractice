5fa8cd8e65898c2f479be6bf3f1cbf461c276f92
Darsicl <scorpion20022007@gmail.com>
2025-12-11 00:16:16 +0100

    repos finished

---
+++ StonksAppWithLogs.Infrastructure/Repositories/StocksRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StonksAppWithLogs.Infrastructure.Repositories
+{
+    internal class StocksRepository
+    {
+    }
+}
